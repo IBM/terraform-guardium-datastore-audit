232cd97a68cae56da9220d34af59f498c8d4e965
Supraja Vempalli <suprajavempalli@ibm.com>
2025-11-17 08:59:30 -0500

    adding versions
    Signed-off-by: Supraja Vempalli <suprajavempalli@ibm.com>

--- examples/aws-aurora-postgres-object/versions.tf
+++ examples/aws-aurora-postgres-object/versions.tf
@@ -6,6 +6,10 @@ terraform {
       source = "IBM/guardium-data-protection"
       version = ">= 1.0.0"
     }
+    gdp-middleware-helper = {
+      source  = "IBM/gdp-middleware-helper"
+      version = ">= 1.0.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.0.0"
