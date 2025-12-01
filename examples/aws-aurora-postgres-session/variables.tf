309e500026acc18b9ac07ef7d1660d9a76373a66
Supraja Vempalli <suprajavempalli@ibm.com>
2025-12-01 12:10:05 -0500

    update the version
    Signed-off-by: Supraja Vempalli <suprajavempalli@ibm.com>

--- examples/aws-aurora-postgres-session/variables.tf
+++ examples/aws-aurora-postgres-session/variables.tf
@@ -141,3 +141,15 @@ variable "use_multipart_upload" {
   description = "Use multipart/form-data upload instead of SFTP (recommended). Set to false to use legacy SFTP method."
   default     = true
 }
+
+variable "profile_upload_directory" {
+  type        = string
+  description = "Directory path for SFTP upload (chroot path for CLI user)"
+  default     = "/upload"
+}
+
+variable "profile_api_directory" {
+  type        = string
+  description = "Full filesystem path for Guardium API to read CSV files"
+  default     = "/var/IBM/Guardium/file-server/upload"
+}
