efc43437d6efbc5176b9c80da92028afe944c32d
Jasmine Multani <jasmine@ibm.com>
2026-04-10 07:11:22 -0400

    add outputs
    Signed-off-by: Jasmine Multani <jasmine@ibm.com>

---
+++ examples/aws-aurora-postgres-object/outputs.tf
@@ -0,0 +1,9 @@
+#
+# Copyright IBM Corp. 2025
+# SPDX-License-Identifier: Apache-2.0
+#
+
+output "reboot_required" {
+  description = "Whether manual reboot is required to enable audit logging"
+  value       = module.datastore-audit_aws-aurora-postgres-object.reboot_required
+}
