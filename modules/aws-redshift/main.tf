67f1b4a3933e171a703c2508028c3962f1d34ade
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:45:06 -0400

    Update main.tf

--- modules/aws-redshift/main.tf
+++ modules/aws-redshift/main.tf
@@ -10,7 +10,7 @@ data "aws_caller_identity" "current" {}
 
 module "common_aws-configuration" {
   source  = "IBM/common/guardium//modules/aws-configuration"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 }
 
 locals {
@@ -166,7 +166,7 @@ resource "null_resource" "configure_logging" {
 # Universal Connector module - using local for testing
 module "gdp_connect-datasource-to-uc" {
   source  = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
-  version = "= 1.3.3"
+  version = "= 1.3.3-beta"
   count   = var.enable_universal_connector ? 1 : 0 # Skip creation when disabled
 
   udc_name       = local.udc_name_safe
