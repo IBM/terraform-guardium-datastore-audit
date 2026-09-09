7cce8572a769353bfdaf2e61bf5ca3214d6b3ff5
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:29:18 -0400

    Update main.tf
    Signed-off-by: Glenn <glenn.lee@ibm.com>

--- modules/aws-aurora-mysql/main.tf
+++ modules/aws-aurora-mysql/main.tf
@@ -16,12 +16,12 @@ locals {
 
 module "common_aws-configuration" {
   source  = "IBM/common/guardium//modules/aws-configuration"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 }
 
 module "common_aurora-mysql-parameter-group" {
   source  = "IBM/common/guardium//modules/aurora-mysql-parameter-group"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 
   aurora_mysql_cluster_identifier = var.aurora_mysql_cluster_identifier
   audit_events                    = var.audit_events
@@ -56,7 +56,7 @@ locals {
 
 module "gdp_connect-datasource-to-uc" {
   source         = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
-  version        = "= 1.3.3"
+  version        = "= 1.3.3-beta"
   count          = var.enable_universal_connector && var.log_export_type == "Cloudwatch" ? 1 : 0
   udc_name       = local.udc_name_safe
   udc_csv_parsed = local.aurora_mysql_csv
