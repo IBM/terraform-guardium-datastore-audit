b56f1522ae6afae1601089481b4a2bb141e11ed8
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:47:03 -0400

    Update main.tf
    Signed-off-by: Glenn <glenn.lee@ibm.com>

--- modules/azure-cosmos-audit/main.tf
+++ modules/azure-cosmos-audit/main.tf
@@ -31,7 +31,7 @@ data "azurerm_storage_account" "checkpoint" {
 # Call diagnostic settings common module
 module "common_azure-cosmos-diagnostic-settings" {
   source  = "IBM/common/guardium//modules/azure-cosmos-diagnostic-settings"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 
   cosmos_account_name              = var.cosmos_account_name
   resource_group_name              = var.resource_group_name
@@ -69,7 +69,7 @@ locals {
 
 module "common_azure-eventhub-registration" {
   source  = "IBM/common/guardium//modules/azure-eventhub-registration"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 
   # Profile Configuration
   profile_definition_name = "Azure Cosmos over Event Hub"
