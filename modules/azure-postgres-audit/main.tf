b9259a756c8f7da59f244905ccff1e514b8fbd6a
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:49:30 -0400

    Update main.tf

--- modules/azure-postgres-audit/main.tf
+++ modules/azure-postgres-audit/main.tf
@@ -164,7 +164,7 @@ locals {
 
 module "common_azure-eventhub-registration" {
   source  = "IBM/common/guardium//modules/azure-eventhub-registration"
-  version = "= 1.6.1"
+  version = "= 1.6.1-beta"
 
   # Profile Configuration
   profile_definition_name = "Azure Postgres Over Event Hub"
