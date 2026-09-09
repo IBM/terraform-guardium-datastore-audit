86b67df433635ca3890e8394eeb628c42c7c6a65
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:55:25 -0400

    Update main.tf
    Signed-off-by: Glenn <glenn.lee@ibm.com>

--- modules/onprem-mysql/main.tf
+++ modules/onprem-mysql/main.tf
@@ -39,7 +39,7 @@ locals {
 
 module "gdp_connect-datasource-to-uc" {
   source         = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
-  version        = "= 1.3.3"
+  version        = "= 1.3.3-beta"
   count          = var.enable_universal_connector ? 1 : 0
   udc_name       = local.udc_name
   udc_csv_parsed = local.onprem_mysql_csv
